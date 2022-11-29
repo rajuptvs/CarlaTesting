642092373ab9bfd98a98e8a70fa3b55698dfb313
rajuptvs <raju.ptvs@gmail.com>
2022-11-29 08:05:00 -0800

    Walkers and World tick

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ import time
 # from load_world import test
 
 ####
-from spawn_actors import spawn_actors
+from spawn_actors import getVehicles,getWalkers
 try:
     sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
@@ -24,12 +24,38 @@ try:
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
 except IndexError:
     pass
+print(sys.path)
 import carla
 
 def main():
-    print("hello_world")
-    world=spawn_actors()
-    world.spawnme(30)
+    #initial settings
+    client = carla.Client('localhost', 2000)
+    client.set_timeout(10.0)
+    world = client.get_world()
+    avail_maps = client.get_available_maps()
+    world = client.load_world("Town02")
+    blueprint_library = world.get_blueprint_library()
+    settings = world.get_settings()
+    settings.fixed_delta_seconds = 0.03
+    settings.synchronous_mode = True
+    world.set_pedestrians_cross_factor(0.0)
+    world.apply_settings(settings)
+    tm = client.get_trafficmanager(8000)
+    tm.set_synchronous_mode(True)
+
+    blueprintsVehicles = blueprint_library.filter('vehicle.*')
+    vehicles_spawn_points = world.get_map().get_spawn_points()
+    blueprintsWalkers = blueprint_library.filter('walker.pedestrian.*')
+    walker_controller_bp = blueprint_library.find('controller.ai.walker')
+    walkers_spawn_points = world.get_random_location_from_navigation()
+    lidar_segment_bp = blueprint_library.find('sensor.lidar.ray_cast_semantic')
+
+    w_all_actors, w_all_id = getWalkers(client, world, blueprintsWalkers, 10)
+    v_all_actors, v_all_id = getVehicles(client, world, vehicles_spawn_points, blueprintsVehicles, 20)
+    
+    while True:
+        world.tick()
+
 
 if __name__ == '__main__':
     main()
--- spawn_actors.py
+++ spawn_actors.py
@@ -21,78 +21,70 @@ import argparse
 import logging
 from numpy import random
 
-class spawn_actors:
-    def spawnme(self,number=20):
-        vehicles_list = []
-        walkers_list = []
-        all_id = []
-        self.client = carla.Client('127.0.0.1', 2000)
-        self.client.set_timeout(10.0)
-        synchronous_master = False
-        number_of_vehicles=number   
+def getVehicles(client, world, spawn_points, blueprintsVehicles, number):
+    print("Spawning vehicles...")
+    batch= []
+    for i in range(number):
+        spawn_point = random.choice(spawn_points)
+        vehicle_bp = random.choice(blueprintsVehicles)
+        batch.append(carla.command.SpawnActor(vehicle_bp, spawn_point).then(carla.command.SetAutopilot(carla.command.FutureActor, True)))
+    results = client.apply_batch_sync(batch, True)
 
-        try:
-            world = self.client.get_world()
-
-            traffic_manager = self.client.get_trafficmanager(8000)
-            traffic_manager.set_global_distance_to_leading_vehicle(2.5)
-            settings = world.get_settings()
-            traffic_manager.set_synchronous_mode(True)
-            synchronous_master = True
-            settings.synchronous_mode = True
-            settings.fixed_delta_seconds = 0.05
-
-            blueprints = get_actor_blueprints(world, 'vehicle.*', 'All')
-            blueprintsWalkers = get_actor_blueprints(world, 'walker.pedestrian.*','2')
-            blueprints = sorted(blueprints, key=lambda bp: bp.id)
-            spawn_points = world.get_map().get_spawn_points()
-            number_of_spawn_points = len(spawn_points)
-            if number_of_vehicles < number_of_spawn_points:
-                random.shuffle(spawn_points)
-            elif number_of_vehicles > number_of_spawn_points:
-                msg = 'requested %d vehicles, but could only find %d spawn points'
-                logging.warning(msg, number_of_vehicles, number_of_spawn_points)
-                number_of_vehicles = number_of_spawn_points
-            
-            SpawnActor = carla.command.SpawnActor
-            SetAutopilot = carla.command.SetAutopilot
-            FutureActor = carla.command.FutureActor
-            batch = []
-            for n, transform in enumerate(spawn_points):
-                if n >= number_of_vehicles:
-                    break
-                blueprint = random.choice(blueprints)
-                if blueprint.has_attribute('color'):
-                    color = random.choice(blueprint.get_attribute('color').recommended_values)
-                    blueprint.set_attribute('color', color)
-                if blueprint.has_attribute('driver_id'):
-                    driver_id = random.choice(blueprint.get_attribute('driver_id').recommended_values)
-                    blueprint.set_attribute('driver_id', driver_id)
-                else:
-                    blueprint.set_attribute('role_name', 'autopilot')
-                batch.append(SpawnActor(blueprint, transform)
-                    .then(SetAutopilot(FutureActor, True, traffic_manager.get_port())))
-
-            for response in self.client.apply_batch_sync(batch, synchronous_master):
-                if response.error:
-                    logging.error(response.error)
-                else:
-                    vehicles_list.append(response.actor_id)
-            traffic_manager.global_percentage_speed_difference(30.0)
-            while True:
-                world.tick()
+    all_id = [results[i].actor_id for i in range(len(results))]
+    all_actors = world.get_actors(all_id)
+    return all_actors, all_id
 
 
-        finally:
-                    
-            settings = world.get_settings()
-            settings.synchronous_mode = False
-            settings.no_rendering_mode = False
-            settings.fixed_delta_seconds = None
-            world.apply_settings(settings)
-            print('\ndestroying %d vehicles' % len(vehicles_list))
-            self.client.apply_batch([carla.command.DestroyActor(x) for x in vehicles_list])
-            print('\ndestroying %d walkers' % len(walkers_list))
-            self.client.apply_batch([carla.command.DestroyActor(x) for x in all_id])
+def getWalkers(client, world, blueprintsWalkers, number):
+    print("Spawning walkers...")
+    # 1. Take all the random locations to spawn
+    spawn_points = []
+    for i in range(number):
+        spawn_point = carla.Transform()
+        spawn_point.location = world.get_random_location_from_navigation()
+        if (spawn_point.location != None):
+            spawn_points.append(spawn_point)
 
-            time.sleep(0.5)
+    # 2. Build the batch of commands to spawn the pedestrians
+    batch = []
+    for spawn_point in spawn_points:
+        walker_bp = random.choice(blueprintsWalkers)
+        batch.append(carla.command.SpawnActor(walker_bp, spawn_point))
+
+    # 2.1 apply the batch
+    results = client.apply_batch_sync(batch, True)
+    walkers_list=[]
+    for i in range(len(results)):
+        walkers_list.append({"id": results[i].actor_id})
+
+    # 3. Spawn walker AI controllers for each walker
+    batch = []
+    walker_controller_bp = world.get_blueprint_library().find('controller.ai.walker')
+
+    for i in range(len(walkers_list)):
+        batch.append(carla.command.SpawnActor(walker_controller_bp, carla.Transform(), walkers_list[i]["id"]))
+
+    # 3.1 apply the batch
+    results = client.apply_batch_sync(batch, True)
+    for i in range(len(results)):
+        walkers_list[i]["con"] = results[i].actor_id
+
+    # 4. Put altogether the walker and controller ids
+    all_id=[]
+    for i in range(len(walkers_list)):
+        all_id.append(walkers_list[i]["con"])
+        all_id.append(walkers_list[i]["id"])
+    all_actors = world.get_actors(all_id)
+
+    # wait for a tick to ensure client receives the last transform of the walkers we have just created
+    world.tick()
+
+    # 5. initialize each controller and set target to walk to (list is [controller, actor, controller, actor ...])
+    for i in range(0, len(all_actors),2):
+        # start walker
+        all_actors[i].start()
+        # set walk to random point
+        all_actors[i].go_to_location(world.get_random_location_from_navigation())
+        # random max speed
+        all_actors[i].set_max_speed(1 + random.random())    # max speed between 1 and 2 (default is 1.4 m/s)
+    return all_actors, all_id
